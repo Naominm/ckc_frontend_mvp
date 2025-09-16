73925082522c5d40927a43373da5428daedb1c5e
Naominm <naomimbugua349@gmail.com>
2025-09-16 07:43:24 +0300

    reemoved unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import AuthPage from "./components/auth";
 import CreateOrder from "./pages/createOrder";
 import Profile from "./pages/profile";
